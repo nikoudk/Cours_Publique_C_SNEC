a64e8816dc325d56e98c950762dbd82562520c0c
Nikou Xelon <48733381+nikoudk@users.noreply.github.com>
2020-09-30 22:24:38 +0200

    Add files via upload

---
+++ Suites_6.2.4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){	
+	int Clients;
+	printf("Donnez moi un nombre : \n");
+	scanf("%d",&Clients);
+	if (Clients % 2 == 0)
+	{
+		printf("Pair\n");
+	}
+	else
+	{
+		printf("ImPair\n");
+	}
+}
+/* Pas finis commprend pas l'exercices */
